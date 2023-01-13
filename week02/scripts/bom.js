cf42c379b84ea021dcf5cc0b8521c77003fc8d97
erradical64 <68350618+erradical64@users.noreply.github.com>
2023-01-12 23:13:33 -0700

    Add files via upload

---
+++ week02/scripts/bom.js
@@ -0,0 +1,15 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+
+button.addEventListener('click', () => {
+    if (input.value != '') { 
+
+        
+    }
+    li.append(deleteButton);
+
+    
+});
