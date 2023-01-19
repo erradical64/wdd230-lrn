829353444aeac66af93256d6a79814eb28a617e3
erradical64 <68350618+erradical64@users.noreply.github.com>
2023-01-19 13:46:28 -0700

    Update getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,15 @@
 const d = new Date();
 let yearVariable = d.getFullYear();
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+if (numVisits !== 0){
+    visitsDisplay.textContent = numVisits;
+} else{
+    visitsDisplay.textContent = 'This is your first visit.'
+}
+numVisits++;
+localStorage.setItem("visits-ls", numVisits);
 
 document.querySelector("#year").innerHTML = yearVariable;
 
