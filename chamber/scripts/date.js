4e1ff3bfaca351ef9ec4b30c26ea286ec4300c37
erradical64 <68350618+erradical64@users.noreply.github.com>
2023-01-21 21:34:17 -0700

    Update date.js

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,6 +1,13 @@
 const d = new Date();
 let yearVariable = d.getFullYear();
-
 document.querySelector("#year").innerHTML = yearVariable;
-
 document.querySelector(".lastmod").innerHTML = "Last Modification: "+document.lastModified;
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+if (numVisits !== 0){
+    visitsDisplay.textContent = numVisits;
+} else{
+    visitsDisplay.textContent = 'This is your first visit.'
+}
+numVisits++;
+localStorage.setItem("visits-ls", numVisits);
